241295b63605843c5da24e4ca01d1b8622669056
Gideon <abaaneg@gmail.com>
2022-12-20 09:55:45 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+
+
+//  let countEl = document.getElementById("count")
+//  let saveEl = document.getElementById("save-el")
+//  let count = 0
+
+
+//  function increment() {
+
+//      count += 1
+//     countEl.textContent = count;
+   
+//  }
+
+// function save() {
+//         let saveCount = count + " - "
+//         saveEl.textContent += saveCount
+//         countEl.textContent = 0
+//         count = 0
+
+//         console.log(count)
+           
+// }
+
+// // let username = "Amos"
+// // let message = ' You have three new notifs'
+
+// // name = "Alex"
+// // greeting = "Hi , ton nom est "
+// // myGreeting = greeting + name
+// // console.log(myGreeting)
+
+// // welcomeEl = document.getElementById("welcome");
+
+// // let fName = "Animop"
+// // let greeting = "Welcome back "
+
+// // welcomeEl.innerText = greeting + fName
+// // welcomeEl.innerText = welcomeEl.innerText + " emoji"
+
+
+// let firstName = "Emmanuel "
+// let lastName = "Dollar"
+// let fullname = firstName + lastName
+
+// console.log(fullname)
+// let fname = ' Mainus'
+// let greeting = 'Hi there'
+
+// function hello() {
+//     console.log(greeting + fname )
+// }
+
+// hello()
+
+let myPoints = 3
+
+function add3Points() {
+    myPoints += 3
+}
+
+function remove1Point() {
+    myPoints -= 1
+}
+
+add3Points()
+add3Points()
+add3Points()
+remove1Point()
+remove1Point()
+
+console.log(myPoints);
