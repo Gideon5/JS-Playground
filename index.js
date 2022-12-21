5a724d493163f86d7ee88ac768fafbd0fbcfb8e1
Gideon <abaaneg@gmail.com>
2022-12-21 17:49:27 +0000

    skin on(css update)

--- index.js
+++ index.js
@@ -138,20 +138,29 @@
 //     console.log("Not elgible, you have already gotten one")
 // }
 let firstCard = 10
-let secondCard = 12
+let secondCard = 11
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
+let message = ""
 
-if (sum <= 20) {
-    console.log("Do you want to draw a new card?")
-} else if (sum === 21) {
-    console.log("Wohoo! You've got a Blackjack! ")
-    hasBlackJack = true
-} else {
-    console.log("You're out of the game!")
-    isAlive = false
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card"
+    } 
+    else if (sum === 21) {
+        message= "Woohoo! You've got a blackjack"
+        hasBlackJack = true
+    } 
+    else {
+        message = "You're out of the game"
+        isAlive = false
+    }
+
+    console.log(message)
+
 }
 
-console.log(hasBlackJack)
-console.log(isAlive)
+// console.log(hasBlackJack)
+// console.log(isAlive)
