b32c4c3a7d1fa104aa8f3894cf5809c117f268e6
Gideon <abaaneg@gmail.com>
2023-01-02 15:12:17 +0000

    update math functions

--- index.js
+++ index.js
@@ -149,13 +149,10 @@
 
 
 
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]//card array
-
-let sum = firstCard + secondCard
+let cards = []//card array
+let sum = 0
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 // let sumEl = document.getElementById("sum-el")
@@ -164,11 +161,29 @@ let cardsEl = document.querySelector("#cards-el")
 
 function getRandomCard() 
 {
-    return  Math.floor(Math.random() * 13 + 1)
-     
+    randNumber = Math.floor(Math.random() * 13) + 1
+
+    if (randNumber > 10) {
+        return 10
+    } else if (randNumber === 1){
+
+        return 11
+    }
+    else {
+        return randNumber
+    }
 }
 
 function startGame() {
+
+    isAlive = true
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards [0] = firstCard
+    cards [1] = secondCard
+    sum = firstCard + secondCard
+    
+
     renderGame()
 }
 
@@ -194,7 +209,6 @@ function renderGame() {
 
     messageEl.textContent = message
     sumEl.textContent += sum
-    console.log(firstCard)
     
 
 }
