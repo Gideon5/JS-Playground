f5664d432319c8dd5cb1d0128d88b5ebe25c7135
Gideon <abaaneg@gmail.com>
2023-01-02 12:30:12 +0000

    loops

--- index.js
+++ index.js
@@ -254,9 +254,18 @@
 // for ( let i = 10; i < 101; i+= 10) {
 //     console.log(i)
 // }
-let cards = [7, 4, 9]
+// let cards = [7, 4, 9]
 
-for ( i = 0; i < cards.length; i++ )
- {
-    console.log(cards [i])
+// for ( i = 0; i < cards.length; i++ )
+//  {
+//     console.log(cards [i])
+// }
+
+let sentence = ["Hello ", "my ", "name ", "is ", "Per"]
+let greetingEl = document.getElementById("greeting-el")
+
+
+for (i = 0; i < sentence.length; i++) {
+    console.log(sentence[i])
+    greetingEl.textContent += sentence[i]
 }
