ca2a3141694a5fe1665c1419ecf0ec01fcc86df8
Gideon <abaaneg@gmail.com>
2023-01-04 23:34:24 +0000

    more DOM manipulations

--- index.js
+++ index.js
@@ -477,7 +477,13 @@ function renderLeads() {
     let listItems = ""
 for ( let i = 0; i < myLeads.length; i++){
 
-      listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i]  + "</a></li>"
+      listItems += `
+      <li>
+         <a target='_blank' href='${myLeads[i]}'>
+         ${myLeads[i]}
+         </a>
+      </li>
+      `
     console.log(listItems)
 
   
